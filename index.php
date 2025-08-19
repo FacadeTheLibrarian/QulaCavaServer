55c3dc990c505cc227a2a15a1fde0dd3f9e2d656
Ouchi_Yadori <yadoriouchi@gmail.com>
2025-08-19 14:37:33 +0900

    ログイン修正

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$response = array('message' => '');
+$response = array('message' => '', 'data' => '');
 if (!isset($_POST['username'])) {
     $response['message'] = 'data not set';
     echo json_encode($response, true);
@@ -8,22 +8,25 @@ if (!isset($_POST['username'])) {
 $username = htmlspecialchars($_POST['username']);
 $pdo = new PDO('mysql:host=localhost;dbname=qula_cava;charset=utf8', 'client', 'password');
 try {
-    $count = $pdo->query("SELECT count(*) FROM user_data WHERE user_name = '$username'");
+    $doesUserExists = $pdo->query("SELECT count(*) FROM user_data WHERE user_name = '$username'");
 } catch (Exception $exception) {
     $response['message'] = $exception->getMessage();
     echo json_encode($response, true);
     exit;
 }
-if ($count->fetch() <= 0) {
-    $response['message'] = 'no such username';
+if (!$doesUserExists->fetch()) {
+    $response['message'] = 'no such user';
     echo json_encode($response, true);
     exit;
 }
+
 session_start();
 $_SESSION['username'] = $username;
-$challange = "Hello, world!";
-$_SESSION['challange'] = $challange;
-$response['message'] = "to next";
-$response['challange'] = $challange;
+
+$challenge = 'hello';
+$_SESSION['challenge'] = $challenge;
+
+$response['message'] = '';
+$response['data'] = $challenge;
 echo json_encode($response, true);
 exit;
--- login.php
+++ login.php
@@ -12,28 +12,51 @@ if (!isset($_SESSION['username'])) {
     echo json_encode($response, true);
     exit;
 }
-$pdo = $pdo = new PDO('mysql:host=localhost;dbname=qula_cava;charset=utf8', 'client', 'password');
 $username = $_SESSION['username'];
-$possibleResponse = $_POST['response'];
+$clientResponse = $_POST['response'];
+
+$pdo = new PDO('mysql:host=localhost;dbname=qula_cava;charset=utf8', 'client', 'password');
 try {
     $passphraseQuery = $pdo->query("SELECT passphrase FROM user_data WHERE user_name = '$username'");
 } catch (Exception $exception) {
-    echo $exception->getMessage();
+    $response['message'] = 'internal server error';
+    echo json_encode($response, true);
+    exit;
 }
-$passphrase = $passphraseQuery->fetch()['passphrase'];
-if (!isset($passphrase)) {
+if (empty($passphraseQuery)) {
     $response['message'] = 'no such user';
     echo json_encode($response, true);
     exit;
 }
-$challange = $_SESSION['challange'];
-$responseAnswer = bin2hex($passphrase) . $challange;
-$hashedResponse = hash('sha256', $responseAnswer);
-if ($hashedResponse != $possibleResponse) {
-    $response['message'] = 'something wrong';
+$passphrase = $passphraseQuery->fetchColumn();
+
+$challenge = $_SESSION['challenge'];
+$concatenated = bin2hex($passphrase) . $challenge;
+$expectedResponse = hash('sha256', $concatenated);
+if ($expectedResponse != $clientResponse) {
+    $response['message'] = 'something went wrong';
     echo json_encode($response, true);
     exit;
 }
-$response['message'] = "Hello, user!";
+
+try {
+    $userDataQuery = $pdo->query("SELECT udt.id, uig.user_ingame_name FROM user_data udt INNER JOIN user_ingame_data uig ON udt.id = uig.user_id WHERE udt.user_name = '$username'");
+} catch (Exception $exception) {
+    $response['message'] = 'failed to fetch data';
+    echo json_encode($response, true);
+    exit;
+}
+if (empty($userDataQuery)) {
+    $response['message'] = 'failed to fetch user data';
+    echo json_encode($response, true);
+    exit;
+}
+$userData = $userDataQuery->fetch();
+$userId = $userData['id'];
+$userIngameName = $userData['user_ingame_name'];
+$response['message'] = "Hello, $userIngameName !";
+
+$_SESSION['expectedResponse'] = $expectedResponse;
+$_SESSION['userId'] = $userId;
 echo json_encode($response, true);
 exit;
